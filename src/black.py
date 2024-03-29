921fe3f1820e3a0bcf1adf910dbc664792589722
fernandoarauz12 <fernandoarauz12@gmail.com>
2024-03-29 11:28:39 -0500

    first commit

---
+++ src/black.py
@@ -0,0 +1,3 @@
+def create_list():
+    item_list = ["a", "b", "c", "d", "e"]
+    return item_list
---
+++ test.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+def create_list():
+    item_list = ["a", "b", "c", "d", "e"]
+    return item_list
